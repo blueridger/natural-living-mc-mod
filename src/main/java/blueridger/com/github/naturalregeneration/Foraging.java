cf6e7318d80bbadf6ffbf132ad5f06534119b7fc
Thena Seer <11550396+blueridger@users.noreply.github.com>
2022-03-17 13:42:35 -0700

    0.7 Advancements system and Moon phase foraging

--- src/main/java/blueridger/com/github/naturalregeneration/Foraging.java
+++ src/main/java/blueridger/com/github/naturalregeneration/Foraging.java
@@ -23,18 +23,18 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class Foraging {
 	public Foraging() {
-		dropsByTag.put(BlockTags.LEAVES, new Drops(25).add(Items.STICK, 1, 2).add(Items.FEATHER, 1, 2));
+		dropsByTag.put(BlockTags.LEAVES, new Drops(25, true).add(Items.STICK, 1, 2).add(Items.FEATHER, 1, 2));
 
-		dropsByBlock.put(Blocks.GRAVEL, new Drops(50).add(Items.FLINT, 1, 1));
+		dropsByBlock.put(Blocks.GRAVEL, new Drops(50, false).add(Items.FLINT, 1, 1));
 		dropsByBlock.put(Blocks.TALL_GRASS,
-				new Drops(30).add(Items.STRING, 1, 3).add(Items.WHEAT_SEEDS, 1, 3).add(Items.WHEAT, 1, 3));
+				new Drops(30, true).add(Items.STRING, 1, 3).add(Items.WHEAT_SEEDS, 1, 3).add(Items.WHEAT, 1, 3));
 		dropsByBlock.put(Blocks.GRASS,
-				new Drops(50).add(Items.BEETROOT_SEEDS, 1, 3).add(Items.BEETROOT, 1, 3).add(Items.POTATO, 1, 3));
-		dropsByBlock.put(Blocks.VINE, new Drops(30).add(Items.STRING, 1, 1));
-		dropsByBlock.put(Blocks.DEAD_BUSH, new Drops(6).add(Items.STICK, 1, 1).add(Items.ACACIA_PLANKS, 1, 8));
-		dropsByBlock.put(Blocks.SAND, new Drops(50).add(Items.BONE, 1, 1));
-		dropsByBlock.put(Blocks.CACTUS, new Drops(15).add(Items.APPLE, 1, 1));
-		dropsByBlock.put(Blocks.OAK_LEAVES, new Drops(35).add(Items.APPLE, 1, 1));
+				new Drops(50, true).add(Items.BEETROOT_SEEDS, 1, 3).add(Items.BEETROOT, 1, 3).add(Items.POTATO, 1, 3));
+		dropsByBlock.put(Blocks.VINE, new Drops(30, true).add(Items.STRING, 1, 1).add(Items.VINE, 1, 2));
+		dropsByBlock.put(Blocks.DEAD_BUSH, new Drops(6, true).add(Items.STICK, 1, 1).add(Items.ACACIA_PLANKS, 1, 8));
+		dropsByBlock.put(Blocks.SAND, new Drops(50, false).add(Items.BONE, 1, 1));
+		dropsByBlock.put(Blocks.CACTUS, new Drops(15, true).add(Items.APPLE, 1, 1));
+		dropsByBlock.put(Blocks.OAK_LEAVES, new Drops(35, true).add(Items.APPLE, 1, 1));
 	}
 
 	private static final Logger LOGGER = LogUtils.getLogger();
@@ -51,12 +51,13 @@ public class Foraging {
 //		LOGGER.debug(event.getEntity().getLevel().getBlockState(event.getPos()).getTags().map(tag -> tag.toString())
 //				.reduce("", (acc, tag) -> acc + ", " + tag.toString()));
 
+		int moonPhase = event.getPlayer().getLevel().getMoonPhase();
+		int positionHash = Math.abs(event.getPos().getX() + event.getPos().getY() + event.getPos().getZ());
 		for (Map.Entry<TagKey<Block>, Drops> entry : dropsByTag.entrySet()) {
 			if (event.getEntity().getLevel().getBlockState(event.getPos()).getTags()
 					.anyMatch(Predicate.isEqual(entry.getKey()))) {
 				LOGGER.debug("Found " + entry.getKey());
-				ArrayList<ItemStack> result = entry.getValue()
-						.rollDrops(Math.abs(event.getPos().getX() + event.getPos().getY() + event.getPos().getZ()));
+				ArrayList<ItemStack> result = entry.getValue().rollDrops(positionHash, moonPhase);
 				if (result == null)
 					continue;
 				for (ItemStack itemStack : result) {
@@ -67,8 +68,7 @@ public class Foraging {
 		Drops blockDrops = dropsByBlock.get(event.getEntity().getLevel().getBlockState(event.getPos()).getBlock());
 		if (blockDrops == null)
 			return;
-		ArrayList<ItemStack> result = blockDrops
-				.rollDrops(Math.abs(event.getPos().getX() + event.getPos().getY() + event.getPos().getZ()));
+		ArrayList<ItemStack> result = blockDrops.rollDrops(positionHash, moonPhase);
 		if (result == null)
 			return;
 		for (ItemStack itemStack : result) {
@@ -91,13 +91,15 @@ public class Foraging {
 		}
 
 		private final ArrayList<Drop> drops;
-		private final int nonceMax;
-		private int nonce;
+		private final int hashMax;
+		private final boolean moonAffected;
+		private int hash;
 
-		protected Drops(int nonceMax) {
+		protected Drops(int hashMax, boolean moonAffected) {
 			this.drops = new ArrayList();
-			this.nonceMax = nonceMax;
-			this.nonce = 0;
+			this.hashMax = hashMax;
+			this.hash = 0;
+			this.moonAffected = moonAffected;
 		}
 
 		protected Drops add(Item item, int count, int chance) {
@@ -105,16 +107,41 @@ public class Foraging {
 			return this;
 		}
 
-		protected ArrayList<ItemStack> rollDrops(int nonceCheck) {
-			LOGGER.debug("checking " + (nonceCheck % this.nonceMax) + " against nonce " + nonce);
-			if (nonceCheck % this.nonceMax != this.nonce)
+		/** Smaller return value means more likely to forage. */
+		private static double moonPhaseToModifier(int moonPhase) {
+			LOGGER.debug("moon phase " + moonPhase);
+			switch (moonPhase) {
+			case 0:
+				return 0.75;
+			case 1:
+			case 7:
+				return 1;
+			case 2:
+			case 6:
+				return 1.25;
+			case 3:
+			case 5:
+				return 1.5;
+			default:
+				return 2;
+
+			}
+		}
+
+		protected ArrayList<ItemStack> rollDrops(int hashCheck, int moonPhase) {
+			double moonModifier = moonPhaseToModifier(moonPhase);
+			int modifiedHashMax = (int) Math.ceil(moonModifier * this.hashMax);
+
+			LOGGER.debug("checking " + (hashCheck % this.hashMax) + " against hash " + hash + ", max " + this.hashMax
+					+ ". Modified to " + (hashCheck % modifiedHashMax) + " against " + (this.hash % modifiedHashMax)
+					+ ", max " + modifiedHashMax);
+
+			if ((hashCheck % modifiedHashMax) != (this.hash % modifiedHashMax))
 				return null;
-			LOGGER.debug("nonce check succeeded");
-			this.nonce = random.nextInt(this.nonceMax);
+			this.hash = random.nextInt(this.hashMax);
 			ArrayList<ItemStack> result = new ArrayList();
 			for (Drop drop : this.drops) {
 				if (Utils.oneIn(drop.chance)) {
-					LOGGER.debug("adding drop");
 					result.add(new ItemStack(drop.item, drop.count));
 				}
 			}
